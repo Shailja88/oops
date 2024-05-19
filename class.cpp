51e1d69059c6cd6ee9a9da0d89d19540cc167d70
Shailja <genieshailja@gmail.com>
2024-05-19 16:42:15 +0530

    Create class.cpp

---
+++ class.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Hello{
+  public: 
+    void print(){
+     cout<<" HI i am print function of hello class "<<endl;
+    }
+};
+int main(){
+  Hello obj;
+  obj.print();
+}
